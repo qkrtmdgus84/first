b0db1f56907275f282d116c0c61557c861922d07
admin <admin@DESKTOP-5PIFKNV>
2019-06-27 10:09:18 +0900

    Second Commit

---
+++ src/main/java/first/sample/controller/LoginController.java
@@ -0,0 +1,17 @@
+package first.sample.controller;
+
+import org.springframework.stereotype.Controller; 
+import org.springframework.ui.Model; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMethod; 
+
+@Controller @RequestMapping(value = "/login") 
+public class LoginController { 
+	@RequestMapping(value = "/login", method = RequestMethod.GET) 
+	public String loginForm(Model model) throws Exception { 
+//		model.addAttribute("userVO", new UserVO()); 
+		return "login/login"; 
+		
+	} 
+	
+}
